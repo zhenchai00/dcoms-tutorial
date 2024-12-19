e0e1685bf0bc05a1d9437fa6ef666c8986bdeac6
caizhen <zhenchai0000@gmail.com>
2024-12-19 12:08:39 +0800

    update: middleware file

--- DCOMS-Lab2-RMI/src/dcoms/lab2/rmi/Register.java
+++ DCOMS-Lab2-RMI/src/dcoms/lab2/rmi/Register.java
@@ -2,10 +2,11 @@ package dcoms.lab2.rmi;
 
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
-public class Registry {
+public class Register {
     public static void main (String args[]) throws RemoteException {
-        java.rmi.registry.Registry reg = LocateRegistry.createRegistry(1040);
+        Registry reg = LocateRegistry.createRegistry(1040);
         reg.rebind("add", new Server());
     }
 }
--- DCOMS-Lab2-RMI/src/dcoms/lab2/rmi/Server.java
+++ DCOMS-Lab2-RMI/src/dcoms/lab2/rmi/Server.java
@@ -3,6 +3,9 @@ package dcoms.lab2.rmi;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+// unicastremoteobject is a class that provides support for point-to-point active object references
+// and for the activation of objects by remote clients
+// making this Server object as remote object
 public class Server extends UnicastRemoteObject implements rmiinterface {
     public Server() throws RemoteException {
         super();

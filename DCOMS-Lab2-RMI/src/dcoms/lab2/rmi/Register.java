cf7281f26f9595f61b0ba2f0a9a497e77e67afde
caizhen <zhenchai0000@gmail.com>
2025-01-03 20:49:16 +0800

    update: accessible for remote host

--- DCOMS-Lab2-RMI/src/dcoms/lab2/rmi/Register.java
+++ DCOMS-Lab2-RMI/src/dcoms/lab2/rmi/Register.java
@@ -6,7 +6,8 @@ import java.rmi.registry.Registry;
 
 public class Register {
     public static void main (String args[]) throws RemoteException {
-        Registry reg = LocateRegistry.createRegistry(1050);
+        System.setProperty("java.rmi.server.hostname", "192.168.0.11");
+        Registry reg = LocateRegistry.createRegistry(5500);
         reg.rebind("add", new Server()); // register the Server object with the name "add"
         reg.rebind("subtract", new Server()); // register the Server object with the name "subtract"
         reg.rebind("calculateCircleArea", new Server()); // register the Server object with the name "calculateCircleArea"
